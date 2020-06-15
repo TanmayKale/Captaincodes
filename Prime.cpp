a0a18f23f89f13f10820bc4bea7d4d5a7bda55a4
TanmayKale <53813298+TanmayKale@users.noreply.github.com>
2020-06-15 19:33:22 +0530

    Add files via upload

---
+++ Prime.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int a=456;
+main ()
+{
+	if ( a %2==0){
+		printf("The given number is not prime ");}
+		else {
+		printf("The given number is a prime number");
+	}
+}
